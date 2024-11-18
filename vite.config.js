1c7638742dd850fe1672c78a0ec0a4996c348d6b
Andrey9019 <1234andrey5678@gmail.com>
2024-11-18 20:14:01 +0200

    Fix paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/yacht-adventures/",
   plugins: [react()],
+  base: "/yacht-adventures/",
 });
