97ef70a6e5798d55d347d54a54c7d7f7d2f9e8ad
Andrey9019 <1234andrey5678@gmail.com>
2024-11-18 15:37:20 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <p className="text-6xl">Hello, Tailwind!</p>
+    </div>
+  );
+}
+
+export default App;
